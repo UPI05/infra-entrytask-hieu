e06ed10b38c76fc8ce846e9f0d63ce6937304046
Hieu <votruongtrunghieu@gmail.com>
2021-08-05 15:28:36 +0700

    5

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("*", (req, res) => {
   res.status(200).send("<h1>Hello World </h1>");
 });
 
-const PORT = process.env.PORT | "8000";
+const PORT = process.env.PORT | "8080";
 app.listen(PORT, () => {
   console.log("Listen on port: " + PORT);
 });
